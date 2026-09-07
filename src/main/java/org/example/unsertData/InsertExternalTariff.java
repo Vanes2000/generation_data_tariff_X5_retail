2a38a235bbc4018d11c6a9a801bc889279990d03
LAPTOP-U5566HPE\Huawei <vanekkirillov2000@gmail.com>
2026-09-07 23:28:02 +0400

    Реализация механизма для вставки данных в БД

---
+++ src/main/java/org/example/unsertData/InsertExternalTariff.java
@@ -0,0 +1,8 @@
+package org.example.unsertData;
+
+public class InsertExternalTariff {
+
+    private String insertExternaltTariff = "INSERT INTO external_level_tariff(territory_id, zone_id, city_id, object_id, doljnost_id, external_rate, 1_rate, 2_rate, 3_rate, 4_rate, date_from, date_to, date_create, type_from, from_id, account_id, is_active)" +
+            "VALUES (territory, zone, city, object_x5, doljnost_x5, base_rate, rate_1, rate_2, rate_3, rate_4, date_from, date_to, date_create, '3', '1', '1', '1');";
+
+}
---
+++ src/main/java/org/example/unsertData/TestData.java
@@ -0,0 +1,25 @@
+package org.example.unsertData;
+
+import java.util.List;
+
+public class TestData {
+
+    // ID
+    public static final int TERRITORY_ID = 1;
+    public static final int ZONE_ID = 1;
+    public static final int CITY_ID = 12;
+    public static final List<Integer> OBJECT_X5 = List.of(1, 2, 3, 4, 5);
+    public static final int COMPANY_ID = 1;
+
+    // Ставки
+    public static final double EXTERNAL_RATE = 12.2;
+    public static final double ONE_RATE = 22.2;
+    public static final double TWO_RATE = 32.2;
+    public static final double THREE_RATE = 42.2;
+    public static final double FOUR_RATE = 52.2;
+
+    // Даты
+    public static final String DATE_FROM = "2026-01-01";
+    public static final String DATE_TO = "2026-12-31";
+    public static final String DATE_CREATE = "2026-12-31";
+}
